1bf84f3708297ea058a5ad804491621c204d068b
Daniel L <30551610+Daniel-Lescouflair@users.noreply.github.com>
2021-07-30 12:02:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+    response.end("Hello Everyone, this is kind of awesome! Not sure if I understand Docker but yeah. One step at a time!");
 });
 
 const port = process.env.PORT || 1337;
